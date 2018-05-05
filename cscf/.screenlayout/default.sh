3ffcea83859423edcdb76031547719e3e7684164
Kieran Colford <kieran@kcolford.com>
2018-05-05 11:23:55 -0400

    Refactored configuration.

--- cscf/.screenlayout/default.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-0 --mode 1680x1050 --pos 3600x0 --rotate normal --output DVI-I-1 --mode 1680x1050 --pos 0x0 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --primary --mode 1920x1080 --pos 1680x0 --rotate normal
